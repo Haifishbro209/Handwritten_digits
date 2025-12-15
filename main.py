8041a51aba948fd24b29cd7aac097af5a533eab2
haifishbro209 <kaloyannikolov016@gmail.com>
2025-12-15 22:00:21 +0100

    runns hidden layer defines 10 output neurons

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import json
 from random import random
 
 
-with open("weights.json") as f:
-    weights = json.load(f) #and biases
+with open("hidden_layer.json") as f:
+    weights = json.load(f) #weights and biases
 
-print(weights)
+with open("output_layer.json") as f:
+    output_weights = json.load(f) 
 
 dataset = datasets.MNIST(
     root="./data",
@@ -17,13 +18,27 @@ dataset = datasets.MNIST(
 
 print(dataset)
 img , label = dataset[0]
-print(img)
+img_flat = img.flatten().tolist()
+
+print(img_flat)
 print(label)
 
+def run_hidden_layer(data):#data is a 784 long arr
+    output = []
+    for n in range(len(weights)):
+        bias = weights[n][0][0]
+        result = 0
+        for w in range(0,784):
+            result += (weights[n][1][w]*data[w])
+        #do sth
+        result += bias 
+        output.append(result)
+    print(len(output))
+    return output
 
+run_hidden_layer(img_flat)
 
-
-def init_randomWeights(neurons = 128):
+def init_randomWeights(neurons = 128, input_size = 784):
     # weihts = [[[bias],[weights]]neuron]every neuron
     weights = [[[0],[None]*784] for _ in range(neurons)]
     for n in range(len(weights)):
@@ -33,5 +48,10 @@ def init_randomWeights(neurons = 128):
 
 #weights = init_randomWeights()
 
-with open("weights.json" ,"w") as  f:
+output_weights = init_randomWeights(10,128)
+
+with open("hidden_layer.json" ,"w") as  f:
     json.dump(weights, f)
+
+with open("output_layer.json" ,"w") as  f:
+    json.dump(output_weights, f)    
