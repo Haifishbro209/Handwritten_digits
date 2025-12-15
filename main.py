093a20900e8efbc3811babf8937829faa74f72f8
haifishbro209 <kaloyannikolov016@gmail.com>
2025-12-15 21:17:04 +0100

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from torchvision import datasets, transforms #type:ignore
 import json
+from random import random
+
 
 with open("weights.json") as f:
-    weights = json.load(f)
+    weights = json.load(f) #and biases
 
 print(weights)
 
@@ -18,6 +20,15 @@ img , label = dataset[0]
 print(img)
 print(label)
 
-weights = [6,7,9]
+
+def init_randomWeights(neurons = 128):
+    weights = [[[0],[None]*784] for _ in range(neurons)]
+    for n in range(len(weights)):
+        #weights[n][1] = 
+        pass
+
+neurons = 128
+weights = [[[0],[None]*784] for _ in range(neurons)]
+
 with open("weights.json" ,"w") as  f:
     json.dump(weights, f)
